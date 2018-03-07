d2a74918f1b75de88df8d04de9f1bd882511b23b
tsarified <36619494+tsarified@users.noreply.github.com>
2018-03-07 16:03:31 +0800

    changes in navbar css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route} from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 // COMPONENTS
 import Home from './components/home';
